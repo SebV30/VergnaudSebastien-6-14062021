5a1d26cc893cdea55ba9ec464f2701158f069a7a
Vergnaud <seb2724@gmail.com>
2021-06-24 10:34:40 +0200

    Mise en place Likes - Dislkes

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,6 +1,7 @@
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
+//CRÉATION SAUCE
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
@@ -17,6 +18,7 @@ exports.createSauce = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 };
 
+//MODIFICATION SAUCE
 exports.modifySauce = (req, res, next) => {
     let sauceObject = req.file ? {
         ...JSON.parse(req.body.sauce),
@@ -27,27 +29,60 @@ exports.modifySauce = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 };
 
+//SUPPRESSION SAUCE
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
             const filename = sauce.imageUrl.split('/images/')[1];
             fs.unlink(`images/${filename}`, () => {
                 Sauce.deleteOne({ _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Objet supprimé' }))
+                    .then(() => res.status(200).json({ message: 'Sauce supprimée' }))
                     .catch(error => res.status(400).json({ error }));
             })
         })
         .catch(error => res.status(500).json({ error }));
 };
 
+
+//AFFICHER SAUCE SÉLECTIONNÉE
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
         .catch(error => res.status(404).json({ error }));
 };
 
+//AFFICHER TOUTES LES SAUCES
 exports.getAllSauce = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }));
 };
+
+//GESTION LIKES & DISLIKES
+exports.likeDislikeSauce = (req, res, next) => {
+    const sauceId = req.params.id;
+    const userId = req.body.userId;
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+
+                //DIFFÉRENTS CAS POSSIBLES
+                switch (like) {
+                    case 1:
+                        console.log("l'uitilsateur aime");
+                        break;
+                    case -1:
+                        console.log("l'uitilsateur n'aime pas");
+                        break;
+                    case 0:
+                        console.log("annulation du choix");
+
+                })
+            .catch(error => res.status(404).json({ error }));
+        }
+
+
+
+
+
+
+}
